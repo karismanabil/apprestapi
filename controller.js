eee198d79c7b1a780d40923d958096e259b4b627
Kharisma Nabil <kharismanabil.gmail.com>
2025-03-08 23:02:31 +0700

    tambahan view tabel mahasiswa by id

--- controller.js
+++ controller.js
@@ -2,6 +2,7 @@
 
 var response = require('./res');
 var connection = require('./koneksi');
+const conn = require('./koneksi');
 
 exports.index = function(req, res){
     response.ok("Aplikasi REST API berjalan!",res)
@@ -18,3 +19,18 @@ exports.tampilsemuamahasiswa = function(req,res){
         }
     });
 }
+
+
+// menampilkan semua data mahasiswa berdasarkan id
+exports.tampilberdasarkanid = function(req, res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
+        function(error, rows, fields){
+            if(error){
+                console.log(error);
+            }else{
+                response.ok(rows, res);
+            }
+        }
+    );
+} 
