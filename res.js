4c2ebba02075e67e14ee649b7de361ee9736cdac
Kharisma Nabil <kharismanabil.gmail.com>
2025-03-12 21:47:44 +0700

    add module

--- res.js
+++ res.js
@@ -9,3 +9,35 @@ exports.ok = function(values, res){
      res.json(data);
      res.end();
 }
+
+
+// response untuk nested matkul
+exports.oknested = function (values, res) {
+    // Lakukan akumulasi
+    const hasil = values.reduce((akumulasikan, item) => {
+        // Jika mahasiswa sudah ada di akumulasi
+        if(akumulasikan[item.nama]){
+        // buat var group nama mahasiwa
+            const group = akumulasikan[item.nama];
+            // cek jika isi array adalah matakuliah
+            if(Array.isArray(group.matakuliah)){
+                // tambahkan val ke dalam group matkul
+                group.matakuliah.push(item.matakuliah);
+            }else{
+                group.matakuliah = [group.matakuliah, item.matakuliah];
+            }
+        }
+        else{
+            akumulasikan[item.nama] = item;
+        }
+        return akumulasikan;
+    }, {});
+
+    // Ubah hasil dari objek ke array
+    var data = {
+        status: 200,
+        values: Object.values(hasil) // Konversi objek ke array
+    };
+
+    res.json(data);
+};
--- routes.js
+++ routes.js
@@ -6,10 +6,13 @@ module.exports = function(app) {
     app.route('/').get(jsonku.index);
 
     app.route('/tampil').get(jsonku.tampilsemuamahasiswa);
-
+    
     app.route('/tampil/:id').get(jsonku.tampilberdasarkanid);
-
+    
     app.route('/tambahmahasiswa').post(jsonku.tambahmahasiswa);
     app.route('/ubahmahasiswa').put(jsonku.ubahmahasiswa);
     app.route('/hapus').delete(jsonku.hapusmahasiswa);
+
+    app.route('/tampilmatakuliah').get(jsonku.tampilgroupmatakuliah);
+
 }
