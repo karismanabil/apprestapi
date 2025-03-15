f6b84208df5cdf1f9b60515704c4ac0e7c5a8785
Kharisma Nabil <kharismanabil.gmail.com>
2025-03-15 16:21:21 +0700

    add logjn dan verifikasi

--- middleware/index.js
+++ middleware/index.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var auth = require('./auth');
 var router = express.Router();
+const verifikasi = require('./verifikasi');
 
 router.post('/api/v1/register', auth.registrasi);
+router.post('/api/v1/login', auth.login);
+
+// alamat yg perlu otorisasi
+router.get('/api/v1/rahasia', verifikasi(2), auth.halamanrahasia)
 
 module.exports = router;
